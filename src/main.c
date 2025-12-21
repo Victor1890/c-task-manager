cca34838783e6a0e90fdd7d417cec3be2ec0c8ea
Victor1890 <victorrosario.1890@gmail.com>
2025-12-20 20:20:59 -0400

    Implement task management menu and screen cleaning functionality

--- src/main.c
+++ src/main.c
@@ -1,7 +1,56 @@
 #include <stdio.h>
+#include "view.h"
 
 int main(void)
 {
-    printf("Hello, Task Manager!\n");
+    view_print_menu();
+
+    int option;
+    scanf("%d", &option);
+
+    view_clean_screen();
+
+    switch (option)
+    {
+        case 1:
+            printf("Add new task selected.\n");
+            break;
+        
+        case 2:
+            printf("View all tasks selected.\n");
+            break;
+
+        case 3:
+            printf("View pending tasks selected.\n");
+            break;
+
+        case 4:
+            printf("View completed tasks selected.\n");
+            break;
+        
+        case 5:
+            printf("Mark task as completed selected.\n");
+            break;
+
+        case 6:
+            printf("Edit task selected.\n");
+            break;
+
+        case 7:
+            printf("Delete task selected.\n");
+            break;
+        
+        case 8:
+            printf("Save tasks selected.\n");
+            break;
+
+        case 0:
+            printf("Exiting the program.\n");
+            break;
+        
+        default:
+            break;
+    }
+
     return 0;
 }
---
+++ src/view.h
@@ -0,0 +1,3 @@
+void view_print_menu();
+
+void view_clean_screen();
